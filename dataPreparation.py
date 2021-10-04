12e6ed2d09f656c86b6e29083e27c72e25df600a
shubhajitbasak <shubhajit.basak@yahoo.com>
2021-10-04 18:06:59 +0100

    data prep

--- dataPreparation.py
+++ dataPreparation.py
@@ -13,8 +13,10 @@ import cv2
 import os
 from util.detect_lm5 import generate5keypoints
 
-image_path = r'C:\Users\sbasak\Downloads\CASIA-WebFace\Images'
-landmark_path_root = r'C:\Users\sbasak\Downloads\CASIA-WebFace\Detections'
+import warnings
+
+datapath = 'data\CASIA-WebFace'
+# landmark_path_root = 'data\CASIA-WebFace'
 
 warnings.filterwarnings("ignore")
 
@@ -25,6 +27,21 @@ parser.add_argument('--img_folder', nargs="+",  # required=True,
                     default=['CASIA-WebFace'], help='folders of training images')  # , 'lfw'
 parser.add_argument('--mode', type=str, default='train', help='train or val')
 opt = parser.parse_args()
+
+
+def generateImageList(dataPath):
+    img_path = os.path.join(dataPath, 'Images')
+    if os.path.exists(os.path.join(dataPath, 'images.txt')):
+        os.remove(os.path.join(dataPath, 'images.txt'))
+    with open(os.path.join(dataPath, 'images.txt'), 'w') as f1:
+        for root, dirs, files in os.walk(img_path):
+            if len(files) > 0:
+                imgList = glob.glob(root + '/' + '*.jpg')
+                for img in imgList:
+                    detect = img.replace('Images', 'Detections').replace('jpg', 'txt')
+                    if os.path.exists(img) and os.path.exists(detect):
+                        print(img)
+                        f1.writelines(img + '\n')  # +','+detect
 
 
 def generate5keypointdata(img_path):
@@ -62,12 +79,19 @@ def data_prepare(folder_list, mode):
 
     # create files that record path to all training data
     msks_list = []
-    for img_folder in folder_list:
-        path = os.path.join(img_folder, 'mask')
-        msks_list += ['/'.join([img_folder, 'mask', i]) for i in sorted(os.listdir(path)) if 'jpg' in i or
-                      'png' in i or 'jpeg' in i or 'PNG' in i]
 
-    imgs_list = [i.replace('mask/', '') for i in msks_list]
+    for img_folder in folder_list:
+        msks_path = os.path.join(img_folder, 'mask')
+        for root, dirs, files in os.walk(msks_path):
+            if len(files) > 0:
+                msks_list += glob.glob(root + '/' + '*.jpg')
+
+    # for img_folder in folder_list:
+    #     path = os.path.join(img_folder, 'mask')
+    #     msks_list += ['/'.join([img_folder, 'mask', i]) for i in sorted(os.listdir(path)) if 'jpg' in i or
+    #                   'png' in i or 'jpeg' in i or 'PNG' in i]
+
+    imgs_list = [i.replace('mask', 'images') for i in msks_list]
     lms_list = [i.replace('mask', 'landmarks') for i in msks_list]
     lms_list = ['.'.join(i.split('.')[:-1]) + '.txt' for i in lms_list]
 
@@ -79,6 +103,9 @@ def data_prepare(folder_list, mode):
 if __name__ == '__main__':
     # generate 5 key points
     # generate5keypoints(image_path)
+
+    # generate image list file
+    # generateImageList(datapath)
 
     print('Datasets:', opt.img_folder)
     data_prepare([os.path.join(opt.data_root, folder) for folder in opt.img_folder], opt.mode)
--- util/detect_lm68.py
+++ util/detect_lm68.py
@@ -3,8 +3,9 @@ import cv2
 import numpy as np
 from scipy.io import loadmat
 import tensorflow as tf
-from utils.preprocess import align_for_lm
+from util.preprocess import align_for_lm
 from shutil import move
+import glob
 
 mean_face = np.loadtxt('util/test_mean_face.txt')
 mean_face = mean_face.reshape([68, 2])
@@ -54,28 +55,47 @@ def load_lm_graph(graph_filename):
 # landmark detection
 def detect_68p(img_path, sess, input_op, output_op):
     print('detecting landmarks......')
-    names = [i for i in sorted(os.listdir(
-        img_path)) if 'jpg' in i or 'png' in i or 'jpeg' in i or 'PNG' in i]
-    vis_path = os.path.join(img_path, 'vis')
-    remove_path = os.path.join(img_path, 'remove')
-    save_path = os.path.join(img_path, 'landmarks')
-    if not os.path.isdir(vis_path):
-        os.makedirs(vis_path)
-    if not os.path.isdir(remove_path):
-        os.makedirs(remove_path)
-    if not os.path.isdir(save_path):
-        os.makedirs(save_path)
+
+    names=[]
+    for root, dirs, files in os.walk(img_path):
+        if len(files) > 0 and not os.path.exists(root.replace('images', 'landmarks')):
+            # os.makedirs(root.replace('images', 'detections'), exist_ok=True)
+            names += glob.glob(root + '/' + '*.jpg')
+
+
+    # with open(os.path.join(img_path, 'images.txt'), 'r') as f1:
+    #     names = f1.readlines()
+
+    # names = [i for i in sorted(os.listdir(
+    #     img_path)) if 'jpg' in i or 'png' in i or 'jpeg' in i or 'PNG' in i]
 
     for i in range(0, len(names)):
-        name = names[i]
-        print('%05d' % (i), ' ', name)
-        full_image_name = os.path.join(img_path, name)
-        txt_name = '.'.join(name.split('.')[:-1]) + '.txt'
-        full_txt_name = os.path.join(img_path, 'detections', txt_name)  # 5 facial landmark path for each image
+        name = names[i].strip()
+        id = os.path.basename(os.path.split(name)[0])
+
+        imgName = os.path.basename(name)
+
+        vis_path = os.path.join(img_path, 'vis', id)
+        remove_path = os.path.join(img_path, 'remove', id)
+        save_path = os.path.join(img_path, 'landmarks', id)
+        if not os.path.isdir(vis_path):
+            os.makedirs(vis_path)
+
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+
+        print('%05d' % (i), ' ', imgName)
+        full_image_name = name  # os.path.join(img_path, name)
+        # txt_name = '.'.join(name.split('.')[:-1]) + '.txt'
+        txt_name = name.split('\\')[-1].split('.')[0] + '.txt'
+        full_txt_name = name.replace('images', 'detections').replace('jpg',
+                                                                     'txt')  # os.path.join(img_path, 'detections', txt_name)  # 5 facial landmark path for each image
 
         # if an image does not have detected 5 facial landmarks, remove it from the training list
         if not os.path.isfile(full_txt_name):
-            move(full_image_name, os.path.join(remove_path, name))
+            if not os.path.isdir(remove_path):
+                os.makedirs(remove_path)
+            move(full_image_name, os.path.join(remove_path, imgName))
             continue
 
             # load data
@@ -84,9 +104,11 @@ def detect_68p(img_path, sess, input_op, output_op):
 
         # if the alignment fails, remove corresponding image from the training list
         if scale == 0:
+            if not os.path.isdir(remove_path):
+                os.makedirs(remove_path)
             move(full_txt_name, os.path.join(
                 remove_path, txt_name))
-            move(full_image_name, os.path.join(remove_path, name))
+            move(full_image_name, os.path.join(remove_path, imgName))
             continue
 
         # detect landmarks
@@ -104,5 +126,5 @@ def detect_68p(img_path, sess, input_op, output_op):
         landmark[:, 1] = img.shape[0] - 1 - landmark[:, 1]
 
         if i % 100 == 0:
-            draw_landmarks(img, landmark, os.path.join(vis_path, name))
+            draw_landmarks(img, landmark, os.path.join(vis_path, imgName))
         save_label(landmark, os.path.join(save_path, txt_name))
